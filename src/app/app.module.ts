11ec25f8d00b834fe9a69f9e95c4b458fcd4024c
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-04-14 11:37:25 -0300

    Se borra componente Prueba de app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
 import { AltaAlumnosComponent } from './alta-alumnos/alta-alumnos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PruebaComponent,
-    AltaAlumnosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, AltaAlumnosComponent ],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
